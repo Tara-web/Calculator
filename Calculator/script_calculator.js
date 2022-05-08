e384c282ca95b8b3a763d3f3ce16c90ed7c78d7e
Tara <85192557+Tara-web@users.noreply.github.com>
2022-05-08 08:55:28 -0400

    Add files via upload
    This is a simple calculator built with HTML, JavaScript and styled with CSS. This calculator could add, subtract, multiply and divide numbers.

---
+++ Calculator/script_calculator.js
@@ -0,0 +1,142 @@
+var number = "0";
+var result_text = document.getElementById("res-text");
+var is_decimal = false;
+var equation = 0, last_event = "";
+
+function update_show(text) {
+    result_text.innerText = text;
+}
+
+function add_digit(digit) {
+    if (number != "0" && number != "NaN" && number != "Cannot divide by zero") number += digit;
+    else {
+        if (digit != ".")
+            number = digit;
+        else
+            number = "0.";
+    }
+
+    update_show(number);
+}
+
+function remove_digit() {
+    last_letter = number[number.length - 1];
+
+    if (number.length > 0) {
+        number = number.substring(0, number.length - 1);
+
+        if (number.length == 0) number = "0";
+        if (last_letter == ".") is_decimal = false;
+    }
+
+    update_show(number);
+}
+
+function decimal() {
+    if (is_decimal == false) {
+        add_digit(".");
+        is_decimal = true;
+    }
+}
+
+function change_sign() {
+    if (parseFloat(number) > 0) number = "-" + number;
+    else number = number.substring(1, number.length);
+
+    update_show(number);
+}
+
+function reset_after_event(event) {
+    number = "0";
+    last_event = event;
+    is_decimal = false;
+
+    update_show(number);
+}
+
+
+function factorial() {
+    if (last_event == "") equation = parseFloat(number);
+    else equation != parseFloat(number);
+
+    reset_after_event("factorial");
+}
+
+
+function sum() {
+    equation += parseFloat(number);
+
+    reset_after_event("sum");
+}
+
+function sub() {
+    if (last_event == "") equation = parseFloat(number);
+    else equation -= parseFloat(number);
+
+    reset_after_event("sub");
+}
+
+function tavan() {
+    if (last_event == "") equation = parseFloat(number);
+    else equation *= parseFloat(number);
+
+    reset_after_event("tavan");
+}
+
+function times() {
+    if (last_event == "") equation = parseFloat(number);
+    else equation *= parseFloat(number);
+
+    reset_after_event("times");
+}
+function show(){
+
+    var i, no, fact;
+    fact=1;
+    no=Number(document.getElementById("show").value);
+    for(i=1; i<=0; i++)  
+    {
+    fact= fact*i;
+    }  
+    document.getElementById("answer").value= fact;
+    }
+    
+
+function divide() {
+    if (last_event == "") equation = parseFloat(number);
+    else {
+        if (parseFloat(number) != 0)
+            equation /= parseFloat(number);
+        else {
+            clear_all();
+            equation = "Cannot divide by zero";
+            update_show(equation);
+        }
+    }
+
+    reset_after_event("divide");
+}
+
+function equation_answer() {
+    if (last_event == "sum") sum();
+    if (last_event == "sub") sub();
+    if (last_event == "times") times();
+    if (last_event == "divide") divide();
+   
+
+    number = equation.toString();
+    equation = 0;
+    is_decimal = false;
+    last_event = "";
+
+    update_show(number);
+}
+
+function clear_all() {
+    number = 0;
+    equation = 0;
+    is_decimal = false;
+    last_event = "";
+
+    update_show(number);
+}
